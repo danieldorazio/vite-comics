65be547ce0de883dca4fdf2846e82d7845eb9181
danieldorazio <danieldorazio.97@gmail.com>
2023-11-23 21:13:00 +0100

    add: AppFoot

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,44 @@
 <script>
+    export default {
+        data () {
+            return {
+                social:
+                {
+                    title: "FOLLOW US",
+                    socialLink:[
+                        "img/footer-facebook.png",
+                        "img/footer-twitter.png",
+                        "img/footer-youtube.png",
+                        "img/footer-pinterest.png",
+                        "img/footer-periscope.png",
+                    ],
+                },
+            };
+        },
 
+        methods: {
+            getImagePath: function(img) {
+                return new URL(`../assets/${img}`, import.meta.url).href;
+            }
+        },
+    };
 </script>
 
 <template>
-    <h1>FOOTER</h1>
+    <section>
+        <div class="container">
+            <button>SIGN-UP NOW</button>
+        
+            <div class="social">
+                <ul>
+                    <li class="title">{{ social.title }}</li>
+                    <li class="icon" v-for="icon in social.socialLink"><img :src="getImagePath(icon)" alt=""></li>
+                </ul>
+            </div>
+        </div>
+    </section>
 </template>
 
-<style scoped>
-    
+<style scoped lang="scss">
+    @use "../style/AppFooter.scss"
 </style>
