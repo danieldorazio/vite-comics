e4a41f4d8bc961ca17bb413864565d162b21ccb0
danieldorazio <danieldorazio.97@gmail.com>
2023-11-24 17:07:49 +0100

    add: jumbotron, cardsComics

---
+++ src/components/AppCardsComics.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    props: {
+        thumb: String,
+        series: String,
+    }
+}
+</script>
+
+<template>
+    <div>
+        <img :src="thumb" alt="">
+        <span>{{ series }}</span>
+    </div>
+</template>
+<style lang="scss" scoped>
+@use "../style/AppCardsComics.scss"
+</style>
